77a8eb7d8c699ff9361cfe98975256ad2a81be51
AnshpreetSingh351 <gujralanshpreetsingh@gmail.com>
2026-04-15 14:30:46 +0530

    .

--- JS/BackButton.js
+++ JS/BackButton.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         closeCard.addEventListener("click", () => {
           // 🔹 Change this path to wherever you want the button to take you
           window.location.href = "/index.html";
-          // Example: "../index.html" or "../SubPages/Tower_A.html"
+          // Example: "../index.html" or "../Subpages/Tower_A.html"
         });
       }
     });
